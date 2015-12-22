1508674773aff0acb051c80308e4a2779c37325a
Alex McLeod <alxmcleod@gmail.com>
2015-12-23 10:08:54 +1100

    Fixed deconstruct method so that it functions correctly in django 1.9

--- denorm/fields.py
+++ denorm/fields.py
@@ -92,6 +92,7 @@ def denormalized(DBField, *args, **kwargs):
         def deconstruct(self):
             name, path, args, kwargs = super(DenormDBField, self).deconstruct()
             super_name, super_path, super_args, super_kwargs = DBField(*args, **kwargs).deconstruct()
+            args.append(self.func)
             return name, super_path, args, kwargs
 
     def deco(func):
